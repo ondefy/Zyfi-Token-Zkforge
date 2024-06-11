5b99d601de1b95d225d74b1806f293dee1b023c2
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2024-06-11 19:49:08 +0200

    test(rewardDistributor.t.sol): test distribute

--- test/03_RewardDistributor.t.sol
+++ test/03_RewardDistributor.t.sol
@@ -11,6 +11,7 @@ contract RewardDistributor_Tester is Test {
     address TEAM_ADDRESS = makeAddr("TEAM_ADDRESS");
     address DEPLOYER_ADDRESS = makeAddr("DEPLOYER_ADDRESS");
     address USER1 = makeAddr("USER1");
+    address USER2 = makeAddr("USER2");
     ZFIToken zfiToken;
     RewardTracker rewardTracker;
     ZFI_test zfiDeployer = new ZFI_test();
@@ -42,7 +43,7 @@ contract RewardDistributor_Tester is Test {
 
     function deployRewardDistributor() public returns(address rewardDistributorAddress){
         vm.startPrank(DEPLOYER_ADDRESS);
-        rewardDistributorAddress = address(new RewardDistributor(address(rewardTracker), address(rewardTracker)));
+        rewardDistributorAddress = address(new RewardDistributor(address(zfiToken), address(rewardTracker)));
         console2.log(rewardDistributorAddress);
         vm.stopPrank();
     }
@@ -60,18 +61,95 @@ contract RewardDistributor_Tester is Test {
     }
 
 
-    function test_distributeRewards() public setGov(TEAM_ADDRESS) {
+    function test_distributeRewardsOneUser() public setGov(TEAM_ADDRESS) {
+        uint256 distributorAmount = 10000 ether;
+        uint256 userAmount = 10 ether;
+        uint256 tokenPerInterval = 1 ether;
         //setAdmin
+        vm.prank(DEPLOYER_ADDRESS);
+        rewardDistributor.setAdmin(TEAM_ADDRESS);
         //updateLastDistributionTime
+        vm.startPrank(TEAM_ADDRESS);
+        rewardDistributor.updateLastDistributionTime();
         //setTokensPerInterval
+        rewardDistributor.setTokensPerInterval(tokenPerInterval);
+        // mint to ZFI tokens to the dirstibutor
+        deal(address(zfiToken), address(rewardDistributor), distributorAmount);
 
         // user deposit in stZFI
+        deal(address(zfiToken), USER1, userAmount);
+        // deposit in rewardTracker
+        vm.startPrank(USER1);
+        zfiToken.approve(address(rewardTracker), userAmount);
+        rewardTracker.stake(address(zfiToken), userAmount);
+        uint256 userBalance = zfiToken.balanceOf(USER1);
+        assertEq(0, userBalance);
         
-
+        vm.warp(block.timestamp+1);
         // assert with pendingRewards
+        uint256 pendingRewards = rewardDistributor.pendingRewards();
+        assertEq(1 ether, pendingRewards);
 
-        //rewardTracker call distribute
+        uint256 claimRewards = rewardTracker.claim(USER1);
+        assertEq(1 ether, claimRewards);
 
+        userBalance = rewardTracker.balanceOf(USER1);
+        assertEq(userBalance + pendingRewards, userBalance);
+        assertEq(userAmount + tokenPerInterval, userBalance);
+    }
+
+    // multiple users
+    function test_distributeRewardsMultipleUsers() public setGov(TEAM_ADDRESS) {
+        uint256 distributorAmount = 10000 ether;
+        uint256 userAmount = 10 ether;
+        uint256 tokenPerInterval = 1 ether;
+        //setAdmin
+        vm.prank(DEPLOYER_ADDRESS);
+        rewardDistributor.setAdmin(TEAM_ADDRESS);
+        //updateLastDistributionTime
+        vm.startPrank(TEAM_ADDRESS);
+        rewardDistributor.updateLastDistributionTime();
+        //setTokensPerInterval
+        rewardDistributor.setTokensPerInterval(tokenPerInterval);
+        // mint to ZFI tokens to the dirstibutor
+        deal(address(zfiToken), address(rewardDistributor), distributorAmount);
+
+        // USER1 deposit in stZFI
+        deal(address(zfiToken), USER1, userAmount);
+        // deposit in rewardTracker
+        vm.startPrank(USER1);
+        zfiToken.approve(address(rewardTracker), userAmount);
+        rewardTracker.stake(address(zfiToken), userAmount);
+        uint256 userBalance = zfiToken.balanceOf(USER1);
+        assertEq(0, userBalance);
+
+        // USER2 deposit in stZFI
+        deal(address(zfiToken), USER2, userAmount);
+        // deposit in rewardTracker
+        vm.startPrank(USER2);
+        zfiToken.approve(address(rewardTracker), userAmount);
+        rewardTracker.stake(address(zfiToken), userAmount);
+        userBalance = zfiToken.balanceOf(USER2);
+        assertEq(0, userBalance);
         
+        vm.warp(block.timestamp+1);
+        // assert with pendingRewards
+        uint256 pendingRewards = rewardDistributor.pendingRewards();
+        assertEq(1 ether, pendingRewards);
+
+        uint256 claimRewards = rewardTracker.claim(USER1);
+        assertEq(tokenPerInterval / 2, claimRewards);
+
+        userBalance = rewardTracker.balanceOf(USER1);
+        assertEq(userAmount + pendingRewards/2, userBalance);
+        assertEq(userAmount + tokenPerInterval/2, userBalance);
+
+        vm.startPrank(USER2);
+        claimRewards = rewardTracker.claim(USER2);
+        assertEq(tokenPerInterval / 2, claimRewards);
+
+        userBalance = rewardTracker.balanceOf(USER2);
+        assertEq(userAmount + pendingRewards/2, userBalance);
+        assertEq(userAmount + tokenPerInterval/2, userBalance);
     }
 }
