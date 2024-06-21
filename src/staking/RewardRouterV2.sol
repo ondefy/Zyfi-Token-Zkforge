47c7a357699e2baba0d711466c566a563e5ae5bc
spider-g <spider@gravitaprotocol.com>
2024-06-21 17:12:10 -0300

    feat: unused var as function return

--- src/staking/RewardRouterV2.sol
+++ src/staking/RewardRouterV2.sol
@@ -80,7 +80,7 @@ contract RewardRouterV2 is ReentrancyGuard, Governable {
         bool _shouldStakeZfi,
         bool _shouldClaimStZfi,
         bool _shouldStakeStZfi
-    ) external nonReentrant {
+    ) external nonReentrant returns (uint256 stZfiAmount) {
         address account = msg.sender;
 
         uint256 zfiAmount = 0;
@@ -92,7 +92,6 @@ contract RewardRouterV2 is ReentrancyGuard, Governable {
             _stakeZfi(account, account, zfi, zfiAmount);
         }
 
-        uint256 stZfiAmount = 0;
         if (_shouldClaimStZfi) {
             stZfiAmount = IRewardTracker(stakedZfiTracker).claimForAccount(account, account);
         }
