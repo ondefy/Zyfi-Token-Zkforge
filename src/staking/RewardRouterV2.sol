0f160f049dc3d4fe495556976af91c3a1740da76
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2024-06-25 23:04:08 +0200

    refactor(RewardRouterV2.sol): remove unnecessary arguments

--- src/staking/RewardRouterV2.sol
+++ src/staking/RewardRouterV2.sol
@@ -23,8 +23,8 @@ contract RewardRouterV2 is ReentrancyGuard, Governable {
 
     mapping (address => address) public pendingReceivers;
 
-    event StakeZfi(address account, address token, uint256 amount);
-    event UnstakeZfi(address account, address token, uint256 amount);
+    event StakeZfi(address account, uint256 amount);
+    event UnstakeZfi(address account, uint256 amount);
 
     function initialize(
         address _zfi,
@@ -47,18 +47,17 @@ contract RewardRouterV2 is ReentrancyGuard, Governable {
     }
 
     function batchStakeZfiForAccount(address[] memory _accounts, uint256[] memory _amounts) external nonReentrant onlyGov {
-        address _zfi = zfi;
         for (uint256 i = 0; i < _accounts.length; i++) {
-            _stakeZfi(msg.sender, _accounts[i], _zfi, _amounts[i]);
+            _stakeZfi(msg.sender, _accounts[i], _amounts[i]);
         }
     }
 
     function stakeZfiForAccount(address _account, uint256 _amount) external nonReentrant onlyGov {
-        _stakeZfi(msg.sender, _account, zfi, _amount);
+        _stakeZfi(msg.sender, _account, _amount);
     }
 
     function stakeZfi(uint256 _amount) external nonReentrant {
-        _stakeZfi(msg.sender, msg.sender, zfi, _amount);
+        _stakeZfi(msg.sender, msg.sender, _amount);
     }
 
     function claimStZfi() external nonReentrant {
@@ -89,7 +88,7 @@ contract RewardRouterV2 is ReentrancyGuard, Governable {
         }
 
         if (_shouldStakeZfi && zfiAmount > 0) {
-            _stakeZfi(account, account, zfi, zfiAmount);
+            _stakeZfi(account, account, zfiAmount);
         }
 
         uint256 stZfiAmount = 0;
@@ -118,15 +117,15 @@ contract RewardRouterV2 is ReentrancyGuard, Governable {
         uint256 stZfiAmount = IRewardTracker(stakedZfiTracker).claimForAccount(_account, _account);
     }
 
-    function _stakeZfi(address _fundingAccount, address _account, address _token, uint256 _amount) private {
+    function _stakeZfi(address _fundingAccount, address _account, uint256 _amount) private {
         require(_amount > 0, "RewardRouter: invalid _amount");
         IRewardTracker(stakedZfiTracker).stakeForAccount(_fundingAccount, _account, _amount);
-        emit StakeZfi(_account, _token, _amount);
+        emit StakeZfi(_account, _amount);
     }
 
-    function _unstakeZfi(address _account, address _token, uint256 _amount) private {
+    function _unstakeZfi(address _account, uint256 _amount) private {
         require(_amount > 0, "RewardRouter: invalid _amount");
         IRewardTracker(stakedZfiTracker).unstakeForAccount(_account, _amount, _account);
-        emit UnstakeZfi(_account, _token, _amount);
+        emit UnstakeZfi(_account, _amount);
     }
 }
